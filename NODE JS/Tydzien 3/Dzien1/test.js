a04f9bfc24d9c0336c010366f0b59612149d3c13
Fremen1990 <fremen1990@gmail.com>
2021-09-27 00:02:56 +0200

    Tydzien 3 dzien 1

--- NODE JS/Tydzien 3/Dzien1/test.js
+++ NODE JS/Tydzien 3/Dzien1/test.js
@@ -1,2 +1,12 @@
-console.log("Hello World!");
-console.error("oh nooo Error!");
+const {exec} = require('child_process');
+
+exec('dir', (error, stdout, stderr)=>{
+    if(error){
+        console.error( "oh nooo Error!");
+    }else if(stderr){
+        console.log("STD ERROR")
+    }else {
+        console.log("Hello World!");
+
+    }
+})
