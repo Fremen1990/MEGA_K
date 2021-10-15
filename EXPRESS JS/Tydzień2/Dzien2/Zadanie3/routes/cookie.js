b0f7e80feea8c8609ebe33ee316df5abf500a8dc
Fremen1990 <fremen1990@gmail.com>
2021-10-16 00:52:15 +0200

    Zadanie3

--- EXPRESS JS/Tydzień2/Dzien2/Zadanie3/routes/cookie.js
+++ EXPRESS JS/Tydzień2/Dzien2/Zadanie3/routes/cookie.js
@@ -2,8 +2,33 @@ const express = require('express');
 const cookieRouter = express.Router();
 
 cookieRouter
+    .post('/set', (req, res) => {
+        const {name} = req.body;
+        // console.log(req.body)
+
+        res.cookie('name', name, {
+            maxAge: 1000 * 60 * 60 * 24 * 30,
+        })
+            .send("Name saved.")
+    })
+
+    .get('/show', (req, res) => {
+        console.log(req.cookies)
+
+        const {name}=req.cookies
+
+        res.send(name === undefined ? "No name" : name)
+    })
+
+.get('/check', (req, res)=>{
+    const {name}= req.cookies;
+
+    res.send(
+        name===undefined? 'There is no name': 'Name is saved')
+})
 
 
 module.exports = {
-    calcRouter: cookieRouter,
+    cookieRouter,
 }
+
