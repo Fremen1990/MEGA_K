47fdcd442b6072c6c8edce4dd5eb4dcf36161cf5
Fremen1990 <fremen1990@gmail.com>
2021-10-16 01:27:30 +0200

    handlebars

--- EXPRESS JS/Tydzień2/Dzien2/Zadanie3/app.js
+++ EXPRESS JS/Tydzień2/Dzien2/Zadanie3/app.js
@@ -2,14 +2,22 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const {cookieRouter} = require('./routes/cookie')
 const app = express()
+const hbs = require('express-handlebars');
 
 app.use(express.json())
 app.use(express.static('public'))
 app.use(cookieParser());
 app.use(express.urlencoded({
-    extended:true,
+    extended: true,
 }));
+app.engine('.hbs', hbs({extname: '.hbs'}));
+app.set('view engine', '.hbs');
+
 
 app.use('/cookie', cookieRouter)
+
+app.get('/hi',(req,res)=>{
+    res.render('home')
+})
 
 app.listen(3000, 'localhost');
